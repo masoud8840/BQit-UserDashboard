2bf5cbb69061d0d1f6001bcf44c466cafce8eb22
masoud8840 <masoudgharedaghi@yahoo.com>
2022-09-02 22:49:15 +0430

    template built (Mobile)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./*'],
+    content: ['./**/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            borderRadius: {
+                '15px': '15px',
+                '25px': '25px'
+            },
+            padding: {
+                '10px': '10px',
+                '30px': '30px'
+            },
+            margin: {
+                '10px': '10px'
+            },
+            colors: {
+                'navigation__text': '#5B5B5B',
+                'navigation__active': "#FF9900",
+                "invite-link": "#5867DD",
+                'normal': '#4A4A4A',
+                'withdrawal-amount': '#F4516C'
+            },
+            backgroundColor: {
+                "navigation__active": "#ff9900",
+                "invite-link": "#5867DD",
+                // 'withdrawal-amount': '#F4516C'
+            },
+        },
     },
     plugins: [],
 }
