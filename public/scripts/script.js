9d62ef9dbc644b6786337d8608db690d71fd3b50
masoud8840 <masoudgharedaghi@yahoo.com>
2022-09-09 14:01:20 +0430

    added some script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,6 +6,10 @@ const toggleDepositRequestsTabs = (clickedTab, htmlEl) => {
     const activeUsersTable = document.querySelector('.active-users__table');
     const historyRequestsTable = document.querySelector('.history-requests__table');
     const pendingUsersTable = document.querySelector('.pending-users__table')
+    const referralProfitTable = document.querySelector('.referral-profit__table');
+    const depositProfitTable = document.querySelector('.deposit-profit__table');
+    const summaryProfitTable = document.querySelector('.summary-profit__table');
+    const summaryBtnArrow = document.querySelector('.summary-btn__arrow-down');
     const tableBtns = document.querySelectorAll(".table__btn");
     tableBtns.forEach(btn => {
         btn.classList.remove('table-btn__active')
@@ -32,6 +36,24 @@ const toggleDepositRequestsTabs = (clickedTab, htmlEl) => {
         pendingUsersTable.style.display = 'flex'
         currentTab = 'pending users'
     }
+    if (clickedTab === 'referral profit') {
+        depositProfitTable.style.display = 'none'
+        // summaryProfitTable.style.display = 'none';
+        referralProfitTable.style.display = 'block'
+        summaryBtnArrow.src = '/public/images/icon/dashboard/Arrow_Down.svg'
+    }
+    if (clickedTab === 'deposit profit') {
+        depositProfitTable.style.display = 'block'
+        // summaryProfitTable.style.display = 'none';
+        referralProfitTable.style.display = 'none'
+        summaryBtnArrow.src = '/public/images/icon/dashboard/Arrow_Down.svg'
+    }
+    if (clickedTab === 'summary profit') {
+        depositProfitTable.style.display = 'none'
+        // summaryProfitTable.style.display = 'block';
+        referralProfitTable.style.display = 'none'
+        htmlEl.childNodes[1].src = '/public/images/icon/dashboard/Arrow_Down_Active.svg'
+    }
 }
 
 // Toggle all rows checkboxes
@@ -40,7 +62,8 @@ const selectAllTableRows = (input) => {
     const historyRequestsTableInputs = document.querySelectorAll('.history-requests__table .table__row input')
     const pendingUsersTableInputs = document.querySelectorAll('.pending-users__table .table__row input')
     const withdrawRequestTableInputs = document.querySelectorAll('.withdraw-requests__table .table__row input');
-    const galleryManagementTableInput = document.querySelectorAll('.gallery-management .table__row input');
+    const galleryManagementTableInputs = document.querySelectorAll('.gallery-management .table__row input');
+    const adminManagementTableInputs = document.querySelectorAll('.admin-management__table .table__row input');
     if (input.checked) {
         if (currentTab === 'active users') {
             activeUsersTableInputs.forEach(input => {
@@ -64,7 +87,12 @@ const selectAllTableRows = (input) => {
         }
 
         if (window.location.pathname === '/views/GalleryManagements.html') {
-            galleryManagementTableInput.forEach(input => {
+            galleryManagementTableInputs.forEach(input => {
+                input.checked = true;
+            })
+        }
+        if (window.location.pathname === '/views/AdminManagement.html') {
+            adminManagementTableInputs.forEach(input => {
                 input.checked = true;
             })
         }
@@ -89,9 +117,15 @@ const selectAllTableRows = (input) => {
                 input.checked = false
             })
         }
-        
+
         if (window.location.pathname === '/views/GalleryManagements.html') {
-            galleryManagementTableInput.forEach(input => {
+            galleryManagementTableInputs.forEach(input => {
+                input.checked = false;
+            })
+        }
+
+        if (window.location.pathname === '/views/AdminManagement.html') {
+            adminManagementTableInputs.forEach(input => {
                 input.checked = false;
             })
         }
@@ -124,4 +158,55 @@ const switchBetween2Tab = (elShouldHide, shouldVisible, toggleVisibleBtn) => {
     const shouldHide = elShouldHide;
     elShouldHide.style.display = 'none';
 
+}
+
+
+
+const switchBetween2Element = (currentRoute) => {
+
+    if (currentRoute === 'withdraw requests') {
+        const withdrawRequestEl = document.querySelector('.withdraw-requests .statistics-table__container');
+        const newWithdrawRequestEl = document.querySelector('.new-withdraw-request');
+        const toggleWithdrawRequestOffBtn = document.querySelector('.withdraw-amount__btn');
+
+        withdrawRequestEl.style.display = 'none';
+        newWithdrawRequestEl.style.display = 'block';
+
+        // send some information to back-end then disappear back
+
+        toggleWithdrawRequestOffBtn.addEventListener("click", () => {
+            newWithdrawRequestEl.style.display = 'block';
+            withdrawRequestEl.style.display = 'none'
+        })
+    }
+    if (currentRoute === 'gallery management') {
+        const galleryManagementEl = document.querySelector('.gallery-management .statistics-table__container');
+        const newGalleryManagementEl = document.querySelector('.new-gallery-image');
+        const toggleOffBtn = document.querySelectorAll('.new-gallery-image button');
+
+        galleryManagementEl.style.display = 'none';
+        newGalleryManagementEl.style.display = 'block'
+
+        toggleOffBtn.forEach(btn => {
+            btn.addEventListener('click', () => {
+                galleryManagementEl.style.display = 'block';
+                newGalleryManagementEl.style.display = 'none'
+            })
+        })
+    }
+    if (currentRoute === 'admin management') {
+        const adminManagementEl = document.querySelector('.admin-management .statistics-table__container');
+        const newAdminManagement = document.querySelector('.new-admin-management');
+        const toggleOffBtn = document.querySelectorAll('.new-admin-management button');
+
+        adminManagementEl.style.display = 'none';
+        newAdminManagement.style.display = 'block'
+
+        toggleOffBtn.forEach(btn => {
+            btn.addEventListener('click', () => {
+                adminManagementEl.style.display = 'block';
+                newAdminManagement.style.display = 'none'
+            })
+        })
+    }
 }
